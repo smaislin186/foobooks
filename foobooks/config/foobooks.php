8eabc386aeaa29aaafa7cab41d5aab79e8ff9c9c
smaislin186 <samantha.maislin@gmail.com>
2017-03-28 21:55:08 -0400

    Practice, controller, environments, other

---
+++ foobooks/config/foobooks.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'defaultNumberOfBooks' => 10,
+];
--- foobooks/routes/web.php
+++ foobooks/routes/web.php
@@ -5,3 +5,17 @@ Route::get('/books', 'BookController@index');
 Route::get('books/{title?}', 'BookController@view');
 
 Route::get('/', 'WelcomeController');
+
+
+/**
+* Log viewer
+* (only accessible locally)
+*/
+if(config('app.env') == 'local') {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+
+/**
+* Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
